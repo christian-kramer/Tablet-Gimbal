0b195d85632cf1ce9dbab62017e59ac071fc7ed6
christian <christian.kramer@mail.com>
2019-11-25 22:31:25 -0600

    successful async UART

---
+++ Electrical/Firmware/PIC16C63A-40 Test.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=PIC16C63A-40_Test.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/PIC16C63A-40_Test.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=pic16c63a-40test.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/pic16c63a-40test.x.tar
